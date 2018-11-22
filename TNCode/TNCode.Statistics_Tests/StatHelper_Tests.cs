18c4300c5d9d69b51a6e364fce137c86fe1cb823
SharpStats <george@sharpstatistics.co.uk>
2018-11-22 21:49:53 +0000

    Added new projects

---
+++ TNCode/TNCode.Statistics_Tests/StatHelper_Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TNCode.Statistics_Tests
+{
+    [TestClass]
+    public class StatHelper_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
